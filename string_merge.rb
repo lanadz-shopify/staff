fb0217a55ff7a59b6ba2a1a0ee4e7f48fab0a429
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2015-01-27 15:00:03 +0200

    merge strings in hashes

---
+++ string_merge.rb
@@ -0,0 +1,15 @@
+module CoreExtensions
+  module Hash
+    module Merging
+      def string_merge(other_hash, separator = " ")
+        merge(other_hash) {|key, old, new| old.to_s + separator + new.to_s}
+      end
+    end
+  end
+end
+
+{}.string_merge({:class => "btn"}) # => {:class=>"btn"}
+
+h = {:class => "btn"} # => {:class=>"btn"}
+h.string_merge({:class => "btn-primary"}) # => {:class=>"btn btn-primary"}
+
